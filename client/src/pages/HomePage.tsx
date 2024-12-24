db83ca473664b2b6351c17e3d79e92a39193fc05
FullStackPete <psliwa.b@gmail.com>
2024-12-24 02:35:56 +0100

    feat: pocketbase init

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,9 +1,35 @@
+import { useEffect } from "react";
 import { ArtSlider } from "../components/ArtSlider";
 import { Wrapper } from "../components/Wrapper";
+import { pb } from "../db/dbconnect";
 
 export default function HomePage() {
+  useEffect(() => {
+    const grabUsers = async () => {
+      const resultList = await pb.collection("painting").getFullList();
+      console.log(resultList);
+    };
+    grabUsers();
+  });
+
+  {
+    /**
+     * TODO: Remove below
+     */
+  }
+  const loginAsAdmin = async () => {
+    const authData = await pb
+      .collection("users")
+      .authWithPassword("test123@test.com", "test1231");
+    console.log(authData);
+    console.log(pb.authStore.isValid);
+    console.log(pb.authStore.token);
+    console.log(pb.authStore.isSuperuser);
+    console.log(pb.authStore.record?.id);
+  };
   return (
     <Wrapper>
+      <button onClick={loginAsAdmin}>login as admin</button>
       <div className="background-secondary montserrat font-extralight text-center text-3xl my-8 md:w-fit md:px-48 w-full py-6 place-self-center text-primary">
         Poznaj moją twórczość
       </div>
