d6b7247c506d50d583287718bf036c71746ad80e
FullStackPete <psliwa.b@gmail.com>
2025-02-01 19:01:14 +0100

    style(ui) temporarily add 2 columns for images

--- client/src/pages/PaintingsPage.tsx
+++ client/src/pages/PaintingsPage.tsx
@@ -5,8 +5,8 @@ import { PrimaryPainting } from "../components/PrimaryPainting";
 
 export function PaintingsPage(): ReactElement {
   return (
-    <Wrapper className="justify-center items-center">
-      <div className="md:mt-12 sm:mt-6">
+    <Wrapper className="justify-center items-center ">
+      <div className="md:mt-12 sm:mt-6 grid lg:grid-cols-2 grid-cols-1 gap-x-16">
         {ITEMS.map((item, index) => (
           <PrimaryPainting
             key={index}
