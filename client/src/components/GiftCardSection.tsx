9b734f6398652dd7b71d26dcecb99322714627a7
FullStackPete <psliwa.b@gmail.com>
2025-03-06 11:56:51 +0100

    feat: enhance mobile view

--- client/src/components/GiftCardSection.tsx
+++ client/src/components/GiftCardSection.tsx
@@ -5,12 +5,8 @@ export interface GiftCardSectionProps {}
 export function GiftCardSection(props: GiftCardSectionProps): ReactElement {
   return (
     <>
-      <div className="flex flex-row w-full gap-16 background-primary">
-        <img
-          src="/miscellaneous/Business Card on Marble Mockup.jpg"
-          className="overflow-hidden object-cover aspect-square w-1/2 object-[75%]"
-        />
-        <div className="w-1/2 flex flex-col gap-y-6 mt-12">
+      <div className="flex flex-col md:flex-row-reverse w-full lg:gap-16 background-primary">
+        <div className="md:w-1/2 flex flex-col gap-y-6 xl:mt-6 md:text-left text-center p-8">
           <p className="font-unna text-5xl font-light">Karta podarunkowa</p>
           <p className="montserrat text-xl font-light leading-8 md:mr-16 mr-4">
             Szukasz pomysłu na prezent? Karta podarunkowa jest uniwersalnym i
@@ -18,10 +14,14 @@ export function GiftCardSection(props: GiftCardSectionProps): ReactElement {
             możliwość samodzielnego wyboru lub zamówienia wymarzonego obrazu.
             Bon można wysłać zarówno w formie elektronicznej, jak i fizycznej.
           </p>
-          <a className="montserrat animate-underline cursor-pointer text-xl w-fit font-medium">
+          <a className="montserrat animate-underline cursor-pointer text-xl w-fit font-medium md:text-left text-center self-center md:self-baseline">
             Więcej
           </a>
         </div>
+        <img
+          src="/miscellaneous/Business Card on Marble Mockup.jpg"
+          className="overflow-hidden object-cover aspect-square md:w-1/2 object-[75%]"
+        />
       </div>
     </>
   );
